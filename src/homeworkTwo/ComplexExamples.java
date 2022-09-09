08a74949a6ab03a9eb2b1f659b12c6d668829e44
NataliyaKurdenkova <natasha_s.86@mail.ru>
2022-09-09 21:48:14 +0300

    fixed the null check in task 1. task 2- redid the algorithm after 1 cycle. task 3 simplified

--- src/homeworkTwo/ComplexExamples.java
+++ src/homeworkTwo/ComplexExamples.java
@@ -1,10 +1,12 @@
 package homeworkTwo;
 
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
 import junit.framework.Assert;
+import org.w3c.dom.ls.LSOutput;
 
 public class ComplexExamples {
 
@@ -72,32 +74,8 @@ public class ComplexExamples {
         System.out.println("Duplicate filtered, grouped by name, sorted by name and id:");
         System.out.println();
 
-//        Person[] RAW_DATA2 = null;
 
-        Map<String, List<Person>> arrayPerson = Arrays.stream(Optional.ofNullable(RAW_DATA)
-                        .orElseThrow(() -> new NullPointerException("Массив пустой")))
-                .distinct()
-                .sorted(Comparator.comparingInt(Person::getId))
-                .collect(Collectors.groupingBy(Person::getName));
-
-
-//        for(Map.Entry<String, List<Person>> item : arrayPerson.entrySet()){
-//
-//            System.out.println(item.getKey()+":");
-//            int i=1;
-//            for(Person person : item.getValue()){
-//
-//                System.out.println(i+" - "+person.getName()+"("+person.getId()+")");
-//                i++;
-//            }
-//            System.out.println();
-//        }
-
-        for (Map.Entry<String, List<Person>> item : arrayPerson.entrySet()) {
-            System.out.println("Key: " + item.getKey());
-            System.out.println("Value: " + item.getValue().size());
-            System.out.println();
-        }
+        printArray(RAW_DATA);
 
         ////////////////////////////////////////////
         ///////TASK 2/////////////
@@ -112,6 +90,7 @@ public class ComplexExamples {
         int target = 10;
         System.out.println(Arrays.toString(twoSum(array, target)));
 
+
         ////////////////////////////////////////////
         ///////TASK 3/////////////
         /////////////
@@ -120,62 +99,94 @@ public class ComplexExamples {
         System.out.println("**************************************************");
         System.out.println("Task 3");
 
-        System.out.println(fuzzySearch("car", "ca6$$#_rtwheel"));// true
+
+        try {
+            System.out.println(fuzzySearch("car", "ca6$$#_rtwheel"));// true
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
 
         TestTask3 testTask3 = new TestTask3();
         testTask3.testEquals();
-
+        }
     }
 
+    public static void printArray(Person[] array) {
+        if (array != null) {
+            Arrays.stream(array)
+                    .filter(Objects::nonNull)
+                    .filter(obj -> obj.getName() != null)
+                    .distinct()
+                    .sorted(Comparator.comparingInt(Person::getId))
+                    .collect(Collectors.groupingBy(Person::getName))
+                    .forEach((k, v) -> System.out.println("Key: " + k + "\n" + "Value: " + v.size()));
+        } else
+            System.out.println("В метод передан пустой массив");
+    }
 
     public static int[] twoSum(int[] array, int target) {
         int[] massiv = new int[2];
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j < array.length; j++) {
-                if (array[i] + array[j] == target) {
+        if (array != null) {
+            int i = 0;
+            int j = array.length - 1;
+            Arrays.sort(array);
+            while (i < j) {
+                 if (array[i] + array[j] == target) {
                     massiv[0] = array[i];
                     massiv[1] = array[j];
                     return massiv;
-                }
+                } else if (array[i] + array[j] < target) i += 1;
+                else if (array[i] + array[j] > target) j -= 1;
             }
-        }
+        } else System.out.println("В метод передан пустой массив");
         return massiv;
     }
-
 
     private static boolean fuzzySearch(String subStr, String str) {
         Boolean contain = true;
 
-        int indexSub = 0;
-        int indexStr = 0;
-        while (indexSub < subStr.length()) {
+        if (subStr.length() != 0 || str.length() != 0 || subStr != null || str != null) {
 
-            String charSub = String.valueOf(subStr.charAt(indexSub));
-            String charStr = String.valueOf(str.charAt(indexStr));
+            int indexSub = 0;
+            int indexStr = 0;
 
-            if (charSub.equals(charStr)) {
-                contain &= (charSub.equals(charStr));
-                indexSub++;
+            while (indexSub < subStr.length()) {
+                Boolean eqSimbol = (subStr.charAt(indexSub) == str.charAt(indexStr));
+
+                if (eqSimbol) {
+                    contain &= eqSimbol;
+                    indexSub++;
+                }
+
+                indexStr++;
+                if ((indexStr >= str.length()) && (indexSub < subStr.length())) {
+                    contain = false;
+                    break;
+                }
             }
-            indexStr++;
-            if ((indexStr >= str.length()) && (indexSub < subStr.length())) {
-                contain = false;
-                break;
-            }
+        } else {
+            System.out.println("В метод переданы нулевые объекты");
+            return false;
         }
+
         return contain;
     }
+
+
 
     static class TestTask3 {
         @Test
         public void testEquals() {
+
             Assert.assertEquals(true, fuzzySearch("car", "ca6$$#_rtwheel"));
             Assert.assertEquals(true, fuzzySearch("cwhl", "cartwheel"));
             Assert.assertEquals(true, fuzzySearch("cartwheel", "cartwheel"));
             Assert.assertEquals(true, fuzzySearch("cwhee", "cartwheel"));
             Assert.assertEquals(false, fuzzySearch("cwheeel", "cartwheel"));
             Assert.assertEquals(false, fuzzySearch("lw", "cartwheel"));
+
 
         }
     }
